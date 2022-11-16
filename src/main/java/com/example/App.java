f87a12695c402ea1f97683f07b830cd0da4055d6
Nitin Gupta <nitinguptaec2008@gmail.com>
2022-11-16 13:10:13 +0530

    nitin gupta logs added

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -19,6 +19,7 @@ public class App {
     {
         Logger log = LoggerFactory.getLogger(App.class);
         log.info("Java app started");
+	log.info("nitin gupta")
     }
 
     public String getStatus() {
